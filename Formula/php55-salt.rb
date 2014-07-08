00dc9a786ddcb49e0543465b7de50d3adf03f4a2
David Steinsland <david@davidsteinsland.net>
2014-07-08 12:50:48 +0200

    Version bump

--- Formula/php55-salt.rb
+++ Formula/php55-salt.rb
@@ -3,8 +3,8 @@ require File.join(File.dirname(__FILE__), 'abstract-php-extension')
 class Php55Salt < AbstractPhp55Extension
   init
   homepage 'https://github.com/SaltShipDesign/salt-util'
-  url 'https://github.com/SaltShipDesign/salt-util/archive/v0.4.2.tar.gz'
-  sha1 '7c99bd61387d74d1a2a172ea51f83b91ec7251ad'
+  url 'https://github.com/SaltShipDesign/salt-util/archive/v0.4.2.1.tar.gz'
+  sha1 '042e5ed1a76759b22bc55109bc1b7d9bfcab43c2'
   head 'https://github.com/SaltShipDesign/salt-util.git'
 
   def install
