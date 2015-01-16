c87dc84a016973e87a2e2f4a2a4a261a97452ffe
David Steinsland <david@davidsteinsland.net>
2015-01-16 15:52:38 +0100

    Added php-scream

---
+++ Formula/php55-scream.rb
@@ -0,0 +1,24 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php55Scream < AbstractPhp55Extension
+  init
+  homepage 'http://pecl.php.net/package/scream'
+  url 'http://pecl.php.net/get/scream-0.1.0.tgz'
+  sha1 '603048320d6e0b714b85ee84c5bb7a874de1eb26'
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+
+    args = []
+    args << "--prefix=#{prefix}"
+    args << phpconfig
+
+    system "./configure", *args
+
+    system "make"
+    prefix.install "modules/scream.so"
+    write_config_file if build.with? "config-file"
+  end
+end
